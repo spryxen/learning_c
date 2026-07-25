9b81ab9d0535420d3f23786913dff08226ebbd89
spryxen <spryxen.2033@gmail.com>
2026-07-25 15:55:45 +0545

    program to print difference or sum using ternary operator

---
+++ ternary_sum_diff.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int number1, number2, result;
+
+  printf("Enter any two numbers: ");
+  scanf("%d%d", &number1, &number2);
+  
+  result = number1 > number2 ? number1 - number2 : number1 + number2;
+
+  printf("The result is %d\n", result);
+
+  return 0;
+
+}
