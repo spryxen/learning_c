ee029ee61ef38a483056eb984fc26b28e39d98d5
spryxen <spryxen.2033@gmail.com>
2026-07-25 19:39:02 +0545

    added divide by zero error.

--- quotient_remainder.c
+++ quotient_remainder.c
@@ -7,10 +7,14 @@ int main(void) {
   printf("Enter dividend and quotient respectively: ");
   scanf("%d %d", &dividend, &divisior);
 
-  quotient = dividend / divisior;
-  remainder = dividend % divisior;
+  if(!divisior) {
+    printf("Divide by zero error.\n");
+  }else {
+    quotient = dividend / divisior;
+    remainder = dividend % divisior;
 
-  printf("Quotient = %d, Remainder = %d.\n", quotient, remainder);
+    printf("Quotient = %d, Remainder = %d.\n", quotient, remainder);
+  }
 
   return 0;
 
