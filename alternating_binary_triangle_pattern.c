cd2b6c0a1c0e88b8fd9b189b7fb9827f64ec4377
spryxen <spryxen.2033@gmail.com>
2026-08-07 20:34:31 +0545

    program to print an alternating binary triangle pattern.

---
+++ alternating_binary_triangle_pattern.c
@@ -0,0 +1,31 @@
+/*
+ 
+1
+01
+101
+0101
+10101
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+	for(int i = 1; i <= 5; i++) {
+
+		for(int j = 1; j <= i; j++) {
+
+			if((i + j) % 2 == 0) 
+				printf("1");
+			else
+				printf("0");
+	
+		}
+
+		printf("\n");
+
+	}
+
+	return 0;
+}
