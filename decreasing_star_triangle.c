448e59dd7803a0408ba2a2be778391fe3ba99ed0
spryxen <spryxen.2033@gmail.com>
2026-08-08 13:53:57 +0545

    program to print a decreasing star triangle pattern.

---
+++ decreasing_star_triangle.c
@@ -0,0 +1,28 @@
+/*
+ 
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+	for(int i = 5; i > 0; i--) {
+
+		for(int j = 1; j <= i; j++) {
+
+			printf("* ");
+			
+		}
+		
+		printf("\n");
+	
+	}
+
+	return 0;
+}
