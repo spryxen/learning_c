258422edbaf0e72623f3e7b074b7f844760406fd
spryxen <spryxen.2033@gmail.com>
2026-08-19 16:38:53 +0545

    Practice floating-point arithmetic

---
+++ 02_practice.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+    float a = 12.4, b = 3.8;
+
+    printf("Sum = %.2f\n", a + b);
+    printf("Difference = %.2f\n", a - b);
+    printf("Product = %.2f\n", a * b);
+    printf("Quotient = %.2f\n", a / b);
+
+    return 0;
+}
