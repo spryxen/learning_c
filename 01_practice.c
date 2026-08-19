8dd7576f710ad89c3ff19fca7e633d3ef46d8a26
spryxen <spryxen.2033@gmail.com>
2026-08-19 16:29:54 +0545

    Initial C project setup

---
+++ 01_practice.c
@@ -0,0 +1,16 @@
+/* Integer arithmetic operations */
+
+#include <stdio.h>
+
+int main(void) {
+
+    int a = 17, b = 4;
+
+    printf("Sum = %d\n", a + b);
+    printf("Difference = %d\n", a - b);
+    printf("Product = %d\n", a * b);
+    printf("Quotient = %d\n", a / b);
+    printf("Remainder = %d\n", a % b);
+
+    return 0;
+}
